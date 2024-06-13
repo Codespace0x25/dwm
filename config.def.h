cc33b875cb1e5c4ce67e625125eca54886ddde44
Olivia <77221503+Codespace0x25@users.noreply.github.com>
2024-06-13 17:27:28 +0000

    made the config and startup programs

--- config.def.h
+++ config.def.h
@@ -34,7 +34,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -45,8 +45,8 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static const char *dmenucmd[] = { "dmenu_run", "-fn", font, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
-static const char *termcmd[]  = { "uxterm", NULL };
+static const char *dmenucmd[] = { "rofi","-show","drun","-show-icons",NULL};
+static const char *termcmd[]  = { "kitty","tmix", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
